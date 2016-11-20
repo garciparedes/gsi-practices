77af6157986e268ec3a0d4c7fffa49dd8af36d7b
garciparedes <sergio@garciparedes.me>
2016-11-20 21:33:26 +0100

    added encrypted files and times

--- practice-4/test-files/cipher.sh
+++ practice-4/test-files/cipher.sh
@@ -6,19 +6,25 @@ CypherAlg=( "aes-256" "des" "camellia-256" "bf")
 CypherMode=( "ecb" "cbc" "cfb")
 
 
+mkdir -p encrypted/$1
+
+
+exec 3>&1 4>&2 >encrypted/$1/readme 2>&1
+
 for i in "${CypherAlg[@]}"
 do
     for j in "${CypherMode[@]}"
     do
-        mkdir -p encrypted/$1
         echo
         echo
         echo
         echo
         echo $1 $i-$j
 
-        time openssl $i-$j -in $1 -out encrypted/$1/$1.$i-$j -k ${password} | tee -a encrypted/$1/readme.md
-        time openssl $i-$j -d -in encrypted/$1/$1.$i-$j -out $1 -k  ${password} | tee -a encrypted/$1/readme.md
-
+        time openssl $i-$j -in $1 -out encrypted/$1/$1.$i-$j -k ${password}
+        time openssl $i-$j -d -in encrypted/$1/$1.$i-$j -out $1 -k  ${password} 
     done
 done
+
+# restore stdout and stderr
+exec 1>&3 2>&4
